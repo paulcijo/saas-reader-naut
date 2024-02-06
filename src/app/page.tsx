1763ad286f5a48164ddeeefbf1fc2b50493bd93d
@cijop <cijop@unicourt.com>
2024-02-06 19:08:17 +0530

    able to create and add book to to-read list;

---
+++ src/app/page.tsx
@@ -0,0 +1,63 @@
+import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
+import { Prisma } from "node_modules/.prisma/client/index";
+
+import { CreatePost } from "~/app/_components/create-post";
+import { api } from "~/trpc/server";
+import { CreateBook } from "./_components/create-book";
+
+export default async function Home() {
+  noStore();
+  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const books = await api.book.getAll.query();
+  console.log(books);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <span className="text-[hsl(280,100%,70%)]">Reader</span>-naut
+        </h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          {books ? (
+            books.map((book) => (
+              <Link
+                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+                href={book.url}
+                target="_blank"
+                key={book.id}
+              >
+                <h3 className="text-2xl font-bold">{book.name}</h3>
+                <div className="text-lg">
+                  {book.url}
+                </div>
+              </Link>
+            ))
+          ) : (
+            <p>You have no books yet.</p>
+          )}
+
+        </div>
+
+        <CreateBook />
+      </div>
+    </main>
+  );
+}
+
+// async function CrudShowcase() {
+//   const latestPost = await api.post.getLatest.query();
+//   const { books } = await api.book.getAll.query();
+//   return (
+//     <div className="w-full max-w-xs">
+//       {books ? (
+//         <p className="truncate">Your most recent post: {latestPost.name}</p>
+//       ) : (
+//         <p>You have no books yet.</p>
+
+//       )}
+
+//       <CreatePost />
+//     </div>
+//   );
+// }
